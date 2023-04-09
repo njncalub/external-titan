6d00151e327af496cff9f7a7f4b7ac11b99605ab
njncalub <njncalub@users.noreply.github.com>
2023-04-09 12:51:38 +0000

    deploy: wandering-inndex/titan@cc7b0626f42b82e6a133e1d20e6c8e2a3fffdcf1

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,18 @@
 import { fileURLToPath } from "url";
 import { configDefaults, defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
+    environment: "jsdom",
     exclude: [...configDefaults.exclude, "**/e2e/**"],
     alias: {
       "~/": fileURLToPath(new URL("./src/", import.meta.url)),
     },
     coverage: {
       provider: "c8",
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "text-summary"],
     },
   },
 });
